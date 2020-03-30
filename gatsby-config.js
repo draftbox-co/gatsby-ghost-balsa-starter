c55f11eb0c7167e264525f11bcb01c23ee0068ad
Keyur <keyur.raval18@gmail.com>
2020-03-30 12:55:11 +0530

    Updated Gatsby Config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,37 +1,98 @@
-let siteConfig
-let ghostConfig
+let siteConfig;
+let ghostConfig;
 
 try {
-    siteConfig = require(`./siteConfig`)
+  siteConfig = require(`./siteConfig`);
 } catch (e) {
-    siteConfig = null
+  siteConfig = null;
 }
 
 try {
-    ghostConfig = require(`./.ghost`)
+  ghostConfig = require(`./.ghost`);
 } catch (e) {
-    ghostConfig = {
-        development: {
-            apiUrl: process.env.GHOST_API_URL,
-            contentApiKey: process.env.GHOST_CONTENT_API_KEY,
-        },
-        production: {
-            apiUrl: process.env.GHOST_API_URL,
-            contentApiKey: process.env.GHOST_CONTENT_API_KEY,
-        },
+  ghostConfig = {
+    development: {
+      apiUrl: process.env.GHOST_API_URL,
+      contentApiKey: process.env.GHOST_CONTENT_API_KEY,
+      version: process.env.GHOST_VERSION
+    },
+    production: {
+      apiUrl: process.env.GHOST_API_URL,
+      contentApiKey: process.env.GHOST_CONTENT_API_KEY,
+      version: process.env.GHOST_VERSION
     }
+  };
 } finally {
-    const { apiUrl, contentApiKey } = process.env.NODE_ENV === `development` ? ghostConfig.development : ghostConfig.production
+  const { apiUrl, contentApiKey } =
+    process.env.NODE_ENV === `development`
+      ? ghostConfig.development
+      : ghostConfig.production;
 
-    if (!apiUrl || !contentApiKey || contentApiKey.match(/<key>/)) {
-        ghostConfig = null //allow default config to take over
-    }
+  if (!apiUrl || !contentApiKey || contentApiKey.match(/<key>/)) {
+    ghostConfig = null; //allow default config to take over
+  }
 }
 
 module.exports = {
-    plugins: [{ resolve: `@armada-inc/gatsby-theme-ghost-attila`,
-        options: {
-            ghostConfig: ghostConfig,
-            siteConfig: siteConfig,
-        } }],
-}
+  plugins: [
+    {
+      resolve: `@armada-inc/gatsby-theme-ghost-attila`,
+      options: {
+        ghostConfig: ghostConfig,
+        siteConfig: siteConfig
+      }
+    },
+    {
+      resolve: `gatsby-plugin-segment-js`,
+      options: {
+        // your segment write key for your production environment
+        // when process.env.NODE_ENV === 'production'
+        // required; non-empty string
+        prodKey: process.env.SEGMENT_KEY,
+
+        // if you have a development env for your segment account, paste that key here
+        // when process.env.NODE_ENV === 'development'
+        // optional; non-empty string
+        devKey: process.env.SEGMENT_KEY,
+
+        // boolean (defaults to false) on whether you want
+        // to include analytics.page() automatically
+        // if false, see below on how to track pageviews manually
+        trackPage: true,
+
+        // boolean (defaults to false); whether to delay load Segment
+        // ADVANCED FEATURE: only use if you leverage client-side routing (ie, Gatsby <Link>)
+        // This feature will force Segment to load _after_ either a page routing change
+        // or user scroll, whichever comes first. This delay time is controlled by
+        // `delayLoadTime` setting. This feature is used to help improve your website's
+        // TTI (for SEO, UX, etc).  See links below for more info.
+        // NOTE: But if you are using server-side routing and enable this feature,
+        // Segment will never load (because although client-side routing does not do
+        // a full page refresh, server-side routing does, thereby preventing Segment
+        // from ever loading).
+        // See here for more context:
+        // GIF: https://github.com/benjaminhoffman/gatsby-plugin-segment-js/pull/19#issuecomment-559569483
+        // TTI: https://github.com/GoogleChrome/lighthouse/blob/master/docs/scoring.md#performance
+        // Problem/solution: https://marketingexamples.com/seo/performance
+        delayLoad: true,
+
+        // number (default to 1000); time to wait after scroll or route change
+        // To be used when `delayLoad` is set to `true`
+        delayLoadTime: 1000
+      }
+    },
+    {
+      resolve: "gatsby-plugin-mailchimp",
+      options: {
+        endpoint:
+        process.env.MC_ENDPOINT // add your MC list endpoint here; see instructions below
+      }
+    },
+    {
+      resolve: `gatsby-plugin-statickit`,
+      options: {
+        siteId: process.env.STATICKIT
+      }
+    }
+  ]
+};
