a178ee26dfe469abd07b179cf86aa93c453c1e93
Keyur <keyur.raval18@gmail.com>
2020-05-15 22:24:16 +0530

    Update site config

--- siteConfig.js
+++ siteConfig.js
@@ -1,15 +1,15 @@
 module.exports = {
-    siteUrl: `https://gatsby-ghost-balsa-starter.now.sh`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://ghost-balsa-preview.draftbox.co/`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Gatsby Ghost Balsa`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `Gastby with Ghost CMS and Balsa Theme`, // This allows an alternative site description for meta data for pages.
+    siteTitleMeta: `Built with Draftbox`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `Lightning fast, secure front-end for your WordPress or Ghost blog, without coding.`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `Ghost`, // Used for App manifest e.g. Mobile Home Screen
+    shortTitle: `Built with Draftbox`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
     themeColor: `#15171A`, // Used for Offline Manifest
